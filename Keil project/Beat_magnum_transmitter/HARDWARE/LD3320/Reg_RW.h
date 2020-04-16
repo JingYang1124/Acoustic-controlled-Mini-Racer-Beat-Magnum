1a83cb282cbe84c4aa3cb92329c5013d4c1fd255
Jing Yang <JingYang@users.noreply.github.com>
2020-04-16 20:38:59 +0800

    Keil project source file

--- Keil project/Beat_magnum_transmitter/HARDWARE/LD3320/Reg_RW.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef REG_RW_H
-#define REG_RW_H
-
-void LD_WriteReg( unsigned char address, unsigned char data);
-unsigned char LD_ReadReg( unsigned char address );
-
-#endif
--- Keil project/Beat_magnum_transmitter/OBJ/core_cm3.d
+++
@@ -1,2 +0,0 @@
-..\obj\core_cm3.o: ..\CORE\core_cm3.c
-..\obj\core_cm3.o: E:\Software\Keil_MDK5\install\ARM\ARMCC\Bin\..\include\stdint.h
